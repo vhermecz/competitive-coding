a35095a3c18f10323d8601c58b05d8aa8ecd0d53
vhermecz <vhermecz@gmail.com>
2021-12-15 06:28:31 +0100

    Solution for AoC'21 day15

---
+++ aoc2021/day15/a0.rb
@@ -0,0 +1,67 @@
+require 'set'
+require 'algorithms'
+
+#INPUT='test'
+INPUT='input'
+
+# read_input
+$data = []
+File.open( INPUT ) do |f|
+	$data = f.map do |row|
+		row.strip.split("").map(&:to_i)
+	end
+end
+
+$h = $data.length
+$w = $data[0].length
+
+NBS = [
+	[1, 0],
+	[-1, 0],
+	[0, 1],
+	[0, -1],
+]
+
+# grow
+ndata = ($h*5).times.map{[0] * $w * 5}
+
+p ndata
+
+5.times do |tx|
+	5.times do |ty|
+		$w.times do |x|
+			$h.times do |y|
+				p [tx,ty,x,y]
+				ndata[ty*$h+y][tx*$w+x] = ($data[y][x] + ty + tx - 1) % 9 + 1
+			end
+		end
+	end
+end
+
+p ndata
+$data = ndata
+$w = 5*$w
+$h = 5*$h
+
+# solve
+
+visited = Hash.new
+expand = Containers::MinHeap.new
+expand.push [0, [0, 0]]
+
+while !expand.empty? do
+	cost, pos = expand.pop
+	break if pos == [$w-1, $h-1]
+	next if visited[pos]
+	visited[pos] = cost
+	NBS.each do |dx,dy|
+		x, y = pos
+		nx, ny = x+dx, y+dy
+		next unless nx >= 0 and nx < $w and ny >= 0 and ny < $h
+		expand.push [cost + $data[ny][nx], [nx, ny]]
+	end
+end
+
+p cost
+# 00:14:55    770 - 540
+# 00:22:28    304 - 2879 (runtime is 37sec, meh)
