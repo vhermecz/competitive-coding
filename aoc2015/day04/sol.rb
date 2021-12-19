3c6eb2ee82c99322d3119de020f4ecc7a3f8b84d
vhermecz <vhermecz@gmail.com>
2021-12-19 19:37:09 +0100

    oops

--- aoc2015/day04/sol.rb
+++ aoc2015/day04/sol.rb
@@ -2,9 +2,9 @@ require 'digest'
 
 puts 1e10.to_i.times do |v|
 	hd = Digest::MD5.hexdigest ("ckczppom" + v.to_s)
-	break v if hd[0,5] == '000000'
+	break v if hd[0,5] == '00000'
 end
 puts 1e10.to_i.times do |v|
 	hd = Digest::MD5.hexdigest ("ckczppom" + v.to_s)
-	break v if hd[0,6] == '00000'
+	break v if hd[0,6] == '000000'
 end
