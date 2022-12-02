ce0977cfcd9cca7573ece6182a69cce3c430c157
vhermecz <vhermecz@gmail.com>
2022-12-02 06:44:27 +0100

    AoC'22 day02 cleanup

--- aoc2022/day02/a0.rb
+++ aoc2022/day02/a0.rb
@@ -7,7 +7,7 @@ data = File.read(INPUT).split("\n").map do |line|
 	line.split(" ")
 end
 
-score = {
+$SCORE = {
 	"A" => 1,
 	"B" => 2,
 	"C" => 3,
@@ -16,31 +16,28 @@ score = {
 	"Z" => 3, # win
 }
 
-data = data.map do |line|
-	e = score[line[0]]
-	y = score[line[1]]
-
-	[(64+e).chr, (65 +(((e - 1) + (y - 2)) % 3)).chr]
-end
-
-p data
-
-d1 = data.map do |line|
-	e = score[line[0]]
-	y = score[line[1]]
-
-	s = if (y==3 and e==2) or(y==2 and e==1) or (y==1 and e==3)
-		6
-	elsif y == e
-		3
-	else
-		0
+def deduct_hand(data)
+	data.map do |line|
+		enemy = $SCORE[line[0]]
+		hint = $SCORE[line[1]]
+		hint_delta = hint - 2
+		your = (((enemy - 1) + (hint_delta)) % 3) + 1
+		[enemy, your].map{|v|(64+v).chr}  # map back to ABC
 	end
-	s + y
 end
 
-p d1
-p d1.sum
+def score(data)
+	d1 = data.map do |line|
+		enemy = $SCORE[line[0]]
+		you = $SCORE[line[1]]
+		score = ((you-enemy+1)%3)*3
+		score + you
+	end
+end
+
+p score(data).sum
+p score(deduct_hand(data)).sum
+
 
 # solve
 # 3:44 start solving
