a27ca5137899ae415462d4c970f33b95ff399660
vhermecz <vhermecz@gmail.com>
2022-12-03 06:40:57 +0100

    AoC'22 day03 cleanup

--- aoc2022/day03/a0.rb
+++ aoc2022/day03/a0.rb
@@ -1,34 +1,34 @@
 require 'set'
 
 INPUT='test'
-INPUT='input'
+#INPUT='input'
 
 # read_input
-data = File.read(INPUT).split("\n").map do |line|
-	a = line.length
-	[line[..a/2-1], line[a/2..]]
-end
+data = File.read(INPUT).split("\n")
 
 def score(x)
 	return (x.ord - 'a'.ord + 1) if x >= 'a'
 	x.ord - 'A'.ord + 1 + 26
 end
 
-b = data.map do |row|
-	e = row.map{|x|x.split("").to_set}
-	common = e[0] & e[1]
-	score(common.to_a[0])
+def commonchar(arr)
+	arr.map{|x|x.split("").to_set}.reduce(:&).to_a.first
 end
 
-p b.sum
-
-data2 = data.map{|x|x[0]+x[1]}
-c= data2.each_slice(3).to_a.map do |row|
-	e = row.map{|x|x.split("").to_set}
-	common = e[0] & e[1] & e[2]
-	score(common.to_a[0])
+def splithalf(str)
+	len = str.length
+	[str[..len/2-1], str[len/2..]]
 end
-p c.sum
+
+p(data.map do |line|
+	splithalf(line)
+end.map do |arr|
+	score(commonchar(arr))
+end.sum)
+
+p(data.each_slice(3).to_a.map do |arr|
+	score(commonchar(arr))
+end.sum)
 
 # solve
 # 10:33 8252 3033rd
