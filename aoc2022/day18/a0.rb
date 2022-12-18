3725df629a1fc4a19d36f8a8903eaae11700a0b7
vhermecz <vhermecz@gmail.com>
2022-12-18 06:39:12 +0100

    AoC'22 day18 cleanup

--- aoc2022/day18/a0.rb
+++ aoc2022/day18/a0.rb
@@ -3,7 +3,7 @@ require 'set'
 #INPUT='test'
 INPUT='input'
 
-NB = [[0, 1, 0], [0, -1, 0], [1, 0, 0], [-1, 0, 0], [0, 0, 1], [0, 0, -1]]
+NBS = [[0, 1, 0], [0, -1, 0], [1, 0, 0], [-1, 0, 0], [0, 0, 1], [0, 0, -1]]
 # read_input
 data = File.read(INPUT).split("\n").map{|r|r.split(",").map(&:to_i)}
 
@@ -19,37 +19,25 @@ def area(data)
 end
 
 def filler(data)
-	vmin = data.flatten.min
-	vmax = data.flatten.max
+	vmin = data.flatten.min-1
+	vmax = data.flatten.max+1
 	seen = Set.new data
-	visit = []
-	(vmin-1..vmax+1).to_a.each do |i|
-		(vmin-1..vmax+1).to_a.each do |j| 
-			visit << [i, j, vmin-1]
-			visit << [i, j, vmax+1]
-			visit << [vmin-1, i, j]
-			visit << [vmax+1, i, j]
-			visit << [i, vmin-1, j]
-			visit << [i, vmax+1, j]
-		end
+	visit = (vmin..vmax).to_a.repeated_permutation(3).filter do |pos|
+		pos.min == vmin or pos.max == vmax
 	end
 	while not visit.empty?
 		pos = visit.pop
 		next if seen.include? pos
 		seen << pos
-		NB.each do |d|
+		NBS.each do |d|
 			npos = pos.zip(d).map(&:sum)
-			next if npos.min < vmin-1 or npos.max > vmax+1
+			next if npos.min < vmin or npos.max > vmax
 			visit << npos
 		end
 	end
-	(vmin-1..vmax+1).each do |i|
-		(vmin-1..vmax+1).each do |j|
-			(vmin-1..vmax+1).each do |k|
-				next if seen.include? [i,j,k]
-				data << [i,j,k]
-			end
-		end
+	(vmin..vmax).to_a.repeated_permutation(3).each do |pos|
+		next if seen.include? pos
+		data << pos
 	end
 end
 
