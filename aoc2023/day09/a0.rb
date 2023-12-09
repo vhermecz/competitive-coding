b0e6192940a42d34cca91ad600829d3a1ae77d6e
vhermecz <vhermecz@gmail.com>
2023-12-09 07:45:57 +0100

    AoC'23 day09

--- aoc2023/day09/a0.rb
+++ aoc2023/day09/a0.rb
@@ -1,51 +1,34 @@
 require 'set'
 
-INPUT='test'
+#INPUT='test'
 INPUT='input'
 
 data = File.read(INPUT).split("\n").map{|row|row.split(" ").map(&:to_i)}
 
-def extrapol seq
-	curr = seq
+def difftree seq
 	seqs = []
+	curr = seq
 	while curr.to_set.to_a != [0]
 		seqs << curr
-		curr = curr.each_cons(2).map do |i1, i2|
-			i2 - i1
-		end
+		curr = curr.each_cons(2).map{|i1, i2|i2 - i1}
 	end
-	seqs.map(&:last).sum
+	seqs
+end
+
+def extrapol seq
+	difftree(seq).map(&:last)
 end
 
 def prepol seq
-	curr = seq
-	seqs = []
-	while curr.to_set.to_a != [0]
-		seqs << curr
-		curr = curr.each_cons(2).map do |i1, i2|
-			i2 - i1
-		end
-	end
-	res = []
 	delta = 0
-	seqs.reverse.each do |pre|
+	difftree(seq).reverse.map do |pre|
 		delta = pre.first - delta
-		res << delta
 	end
-	delta
 end
 
 # solve
-part1 = data.map do |datum|
-	extrapol datum
-end
-
-part2 = data.map do |datum|
-	prepol datum
-end
-
-p part1.sum
-p part2.sum
+p data.map{|datum|extrapol(datum).sum}.sum
+p data.map{|datum|prepol(datum).last}.sum
 
 # Argh, mega super brainfreeze in the morning
 #  9   00:09:16   1079      0   00:15:49   1655      0
